d6f224163aae83deef1228ae069458df8980060c
alex <asb2005@gmail.com>
2020-06-05 13:26:30 -0300

    Alterações do 1, 2 e 3 dia

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const buttonSearch = document.querySelector("#page-home main a")
+const modal = document.querySelector("#modal")
+const close = document.querySelector("#modal .header a")
+
+console.log(buttonSearch)
+buttonSearch.addEventListener("click", () => {
+  modal.classList.remove("hide")
+})
+
+close.addEventListener("click", () => {
+  modal.classList.add("hide")
+})
