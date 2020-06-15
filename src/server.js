f2f29a28d162bdf6292ea1c531cc8bce58b0363a
alex <asb2005@gmail.com>
2020-06-15 11:29:01 -0300

    Alterações do 5 dia

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
 const express = require("express")
 const server = express()
 
+// pegar o banco de dados
+const db = require("./database/db")
+
 // configurar pasta publica
 server.use(express.static("public"))
+
+// habilitar o uso do req.body na aplicação
+server.use(express.urlencoded({ extended: true }))
 
 // utilizando template engine
 const nunjucks = require("nunjucks")
@@ -22,8 +28,63 @@ server.get("/create-point", (req, res) => {
   return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+  
+  // inserir os dados no database
+  const query = `
+    INSERT INTO places (
+      image,
+      name,
+      address,
+      address2,
+      state,
+      city,
+      items
+    ) VALUES (?,?,?,?,?,?,?);
+  `
+  const values = [
+    req.body.image,
+    req.body.name,
+    req.body.address,
+    req.body.address2,
+    req.body.state,
+    req.body.city,
+    req.body.items
+  ]
+
+  function afterInsertData(err) {
+    if(err) {
+      console.log(err)("Erro no cadastro!")
+      return res.send
+    }
+  }
+
+  db.run(query, values, afterInsertData)
+
+
+  return res.render("create-point.html", { saved: true })
+})
+
 server.get("/search", (req, res) => {
-  return res.render("search-results.html")
+  
+  const search = req.query.search
+
+  if(search == "") {
+    return res.render("search-results.html", { total: 0 })
+  }
+
+  // pegar os dados no banco de dados
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+    if(err) {
+      return console.log(err)
+    }
+
+    const total = rows.length
+
+    // Mostrar a página html com os dados do banco de dados
+    return res.render("search-results.html", { places: rows, total })
+  })
+ 
 })
 
 // ligar o servidor
